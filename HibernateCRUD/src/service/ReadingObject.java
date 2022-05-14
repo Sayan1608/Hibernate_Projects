6ea359d16bb6dcd5bb0ec8c42efe79ebf243ecf8
Sayan <subhosen16@gmail.com>
2022-05-14 10:50:40 +0530

    Retrieving Objects from DB

---
+++ HibernateCRUD/src/service/ReadingObject.java
@@ -0,0 +1,40 @@
+package service;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import entity.Teacher;
+import exceptions.TeacherNotFoundException;
+
+public class ReadingObject {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = null;
+		
+		factory = new Configuration().configure().buildSessionFactory();
+		System.out.println("created");
+		
+		System.out.println("Retrieving Teacher...");
+		
+		Transaction tx = null;
+		
+		try(Session session = factory.getCurrentSession()){
+			tx = session.beginTransaction();
+			Teacher theTeacher = session.get(Teacher.class, 5);
+			if(theTeacher == null) {
+				throw new TeacherNotFoundException("No Teacher exists with the given id");
+			}
+			
+			System.out.println("Printing Teacher...\n"+theTeacher);
+			tx.commit();
+			
+		}catch (Exception e) {
+			System.out.println("Error!");
+			e.printStackTrace();
+		}
+	}
+
+}
